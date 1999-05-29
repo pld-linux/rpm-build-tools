d287305cfc69d721486e9974511bf3f714aba182
kloczek <kloczek@pld-linux.org>
1999-05-29 11:02:17 +0000

    - full help, - start implemt parse_spec(), - handle -v, --verbose swith.
    Changed files:
        builder.sh -> 1.4

--- builder.sh
+++ builder.sh
@@ -2,12 +2,24 @@
 # 
 
 PATH="/bin:/usr/bin:/usr/sbin:/sbin:/usr/X11R6/bin"
+
 SPECFILE=""
+BE_VERBOSE=""
+
+PATCHES=""
+SOURCES=""
+ICON=""
+PACKAGE_RELEASE=""
+PACKAGE_VERSION=""
+PACKAGE_NAME=""
 
 usage()
 {
 echo "\
-Usage: builder [-h] [--help] [-q] <package>.spec
+Usage: builder [-V] [--version] [-a] [--as_anon] [-b] [--build]
+	[-d <cvsroot>] [--cvsroot <cvsroot>] [-g] [--get] [-h] [--help]
+	[-l <logfile>] [--logtofile <logfile>] [-q] [--quiet] 
+	[-v] [--verbose] <package>.spec
 
 	-V, --version	- output builder version
 	-a, --as_anon	- get files via pserver as cvs@cvs.pld.org.pl,
@@ -26,18 +38,41 @@ Usage: builder [-h] [--help] [-q] <package>.spec
 
 parse_spec()
 {
+    (echo "%dump"; cat $SPECFILE) > $SPECFILE.__
+
+    SOURCES="`rpm -bp $SPECFILE.__ 2>&1 | awk '/ SOURCE[0-9]+/ {print $3}'`"
+    PATCHES="`rpm -bp $SPECFILE.__ 2>&1 | awk '/ PATCH[0-9]+/ {print $3}'`"
+    ICON="`rpm -bp $SPECFILE.__ 2>&1 | awk '/^Icon:/ {print $2}' ${SPEC}`"
+    PACKAGE_NAME="`rpm -bp $SPECFILE.__ 2>&1 | awk '/ name/ {print $3}'`"
+    PACKAGE_VERSION="`rpm -bp $SPECFILE.__ 2>&1 | awk '/ PACKAGE_VERSION/ {print $3}'`"
+    PACKAGE_RELEASE="`rpm -bp $SPECFILE.__ 2>&1 | awk '/ PACKAGE_RELEASE/ {print $3}'`"
+
+    rm -f $SPECFILE.__
+
+    if [ "$BE_VERBOSE" != "" ]; then
+	echo -e "- Sources :\n  " $SOURCES
+	echo -e "- Patches :\n  " $PATCHES
+	if [ "$ICON" != ""
+	echo -e "- Icon    :\n  " $ICON
+	echo -e "- Name    : " $PACKAGE_NAME
+	echo -e "- Version : " $PACKAGE_VERSION
+	echo -e "- Release : " $PACKAGE_RELEASE
+    fi
 }
 
 get_spec()
 {
+    echo "get_spec"
 }
 
 get_all_files()
 {
+    echo "get_all_files"
 }
 
 build_package()
 {
+    echo "build_package"
 }
 
 while test $# -gt 0 ; do
@@ -59,7 +94,7 @@ while test $# -gt 0 ; do
 	-q | --quiet )
 	    shift ;;
 	-v | --verbose )
-	    shift ;;
+	    BE_VERBOSE="1"; shift ;;
 	* )
 	    SPECFILE="${1}"; shift ;;
     esac
