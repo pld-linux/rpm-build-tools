b48c54b483922cadabbb5bfc0e494943fce676f0
Artur Frysiak <artur@frysiak.net>
1999-06-10 16:34:30 +0000

    != instead -ne
    Changed files:
        builder.sh -> 1.30

--- builder.sh
+++ builder.sh
@@ -148,7 +148,7 @@ get_spec()
 
 get_all_files()
 {
-    if [ "$CVSROOT $PATCHES $ICONS" -ne "  " ]; then
+    if [ "$CVSROOT $PATCHES $ICONS" != "  " ]; then
 	cd $SOURCE_DIR
 
 	if [ -n "$CVSROOT" ]; then
