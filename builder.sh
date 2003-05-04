96686ae5bea66f99255c2d6a3a0f73596320c04f
Arkadiusz Miśkiewicz <arekm@maven.pl>
2003-05-04 11:24:20 +0000

    - sleep for 2 seconds before trying again wich cvs
    Changed files:
        builder.sh -> 1.140

--- builder.sh
+++ builder.sh
@@ -272,6 +272,7 @@ get_spec()
 	    if [ "$result" -ne "0" ]; then
 		if (echo "$output" | grep -qE "(Cannot connect to|connect to .* failed|Connection reset by peer)") && [ "$retries_counter" -le "$CVS_RETRIES" ]; then
 		    echo "Trying again [$SPECFILE]... ($retries_counter)"
+		    sleep 2
 		    continue
 		 fi
 	    	Exit_error err_no_spec_in_repo;
@@ -357,6 +358,7 @@ get_files()
 			[ -n "$output" ] && echo "$output"
 			if (echo "$output" | grep -qE "(Cannot connect to|connect to .* failed|Connection reset by peer)") && [ "$result" -ne "0" -a "$retries_counter" -le "$CVS_RETRIES" ]; then
 				echo "Trying again [`nourl $i`]... ($retries_counter)"
+				sleep 2
 				continue
 			else
 				break
