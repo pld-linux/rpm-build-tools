efd28552643d284e492b138c3530938a6d2c00a7
Arkadiusz Miśkiewicz <arekm@maven.pl>
2004-06-27 13:02:26 +0000

    - just sh, no bashizm
    Changed files:
        adapter.sh -> 1.6

--- adapter.sh
+++ adapter.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 if [ $# -ne 1 -o ! -f "$1" ]; then
   echo "Usage: $0 filename"
@@ -6,17 +6,21 @@ if [ $# -ne 1 -o ! -f "$1" ]; then
 fi
 
 ./adapter.awk "$1" > "$1.adapter"
-diff -u "$1" "$1.adapter"|less
+diff -u "$1" "$1.adapter" | less
 echo -n "Are the changes OK? [yNso] "
 
-read -n 1 OK
-echo
+read OK
 
 if [ "$OK" = "y" -o "$OK" = "Y" ]; then
+  echo "Committing..."
   mv "$1.adapter" "$1"
   cvs ci "$1"
-elif [ "$OK" = "o" -a "$OK" = "O" ];then
+elif [ "$OK" = "o" -o "$OK" = "O" ];then
+  echo "Saving changes..."
   mv "$1.adapter" "$1"
 elif [ "$OK" != "s" -a "$OK" != "S" ];then
+  echo "Ignoring changes..."
   rm "$1.adapter"
+else
+  echo "Did nothing."
 fi
