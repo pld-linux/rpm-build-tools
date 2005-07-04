babc91ef6a8d0e9f9219849d614818fd97c5fe8a
Elan Ruusamäe <glen@pld-linux.org>
2005-07-04 23:17:32 +0000

    - use ./builder for adapterizing
    Changed files:
        adapter.sh -> 1.7

--- adapter.sh
+++ adapter.sh
@@ -5,22 +5,4 @@ if [ $# -ne 1 -o ! -f "$1" ]; then
   exit 1
 fi
 
-./adapter.awk "$1" > "$1.adapter"
-diff -u "$1" "$1.adapter" | less
-echo -n "Are the changes OK? [yNso] "
-
-read OK
-
-if [ "$OK" = "y" -o "$OK" = "Y" ]; then
-  echo "Committing..."
-  mv "$1.adapter" "$1"
-  cvs ci "$1"
-elif [ "$OK" = "o" -o "$OK" = "O" ];then
-  echo "Saving changes..."
-  mv "$1.adapter" "$1"
-elif [ "$OK" != "s" -a "$OK" != "S" ];then
-  echo "Ignoring changes..."
-  rm "$1.adapter"
-else
-  echo "Did nothing."
-fi
+./builder -a "$1"
