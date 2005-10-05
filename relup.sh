b9c37ff0d542c37a21e4257edeadc21460746199
Elan Ruusamäe <glen@pld-linux.org>
2005-10-05 20:09:40 +0000

    - massive commit support
    Changed files:
        relup.sh -> 1.2

--- relup.sh
+++ relup.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 # script to run after "rel up" style change.
 # takes Release from spec and creates commit with message
+# groups similiar commits to gether.
 # "- rel $rel"
 
 set -e
 specfile="$1"
 
 get_dump() {
+	local specfile="$1"
 	rpm --specfile "$specfile" --define 'prep %dump'  -q 2>&1
 }
 
@@ -14,8 +16,16 @@ get_release() {
 	awk '/PACKAGE_RELEASE/{print $NF; exit}'
 }
 
-rel=$(get_dump | get_release)
-echo "Release: $rel"
-if [ "$rel" ]; then
-	cvs ci -m "- rel $rel" $specfile
-fi
+
+tmpd=$(mktemp -d "${TMPDIR:-/tmp}/relXXXXXX")
+for spec in "$@"; do
+	rel=$(get_dump "$spec" | get_release)
+	echo "$spec" >> "$tmpd/$rel"
+done
+
+for file in $(ls "$tmpd" 2>/dev/null); do
+	files=$(cat "$tmpd/$file")
+	rel=$(basename "$file")
+	cvs ci -m "- rel $rel" $files
+done
+rm -rf $tmpd
