595e4fdeafdf873f229a74a3c9a0e711a757823d
Elan Ruusamäe <glen@pld-linux.org>
2006-10-23 20:03:49 +0000

    - restore r1.4
    Changed files:
        repackage.sh -> 1.6

--- repackage.sh
+++ repackage.sh
@@ -11,13 +11,19 @@ set -e
 
 rpmbuild() {
 	set -x
-	/usr/bin/rpmbuild --short-circuit --define '_source_payload w9.gzdio' $bconds "$@" || exit
+	/usr/bin/rpmbuild ${TARGET:+--target $TARGET} --short-circuit --define '_source_payload w9.gzdio' "$@" || exit
 }
 
 bconds=$(./builder --show-bconds "$@")
 # ignore output from older builders whose output is not compatible.
 if [ "$(echo "$bconds" | wc -l)" -gt 1 ]; then
 	bconds=""
+fi
+
+SPECFILE="$1"
+tmp=$(awk '/^BuildArch:/ { print $NF}' $SPECFILE)
+if [ "$tmp" ]; then
+	TARGET="$tmp"
 fi
 
 # just create the rpm's if -bb is somewhere in the args
