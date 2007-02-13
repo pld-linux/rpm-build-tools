4845abe9eb4951ad378745232a9ddf9453a3c807
Elan Ruusamäe <glen@pld-linux.org>
2007-02-13 18:39:07 +0000

    - some usage - skip cvs up just to fetch bcond args
    Changed files:
        compile.sh -> 1.5

--- compile.sh
+++ compile.sh
@@ -2,6 +2,14 @@
 # will build package, skipping %prep and %build stage
 # i use it a lot!
 #
+# Usage:
+# do do only %build stage (ie after %prep has been done), for example after
+# modifying some sources for more complicated specs wholse %build is not just
+# %{__make}:
+# SPECS$ ./compile.sh kdelibs.spec
+#
+# See also: SPECS/repackage.sh
+#
 # -glen 2005-03-03
 
 set -e
@@ -19,6 +27,6 @@ if [ "$tmp" ]; then
 	TARGET="$tmp"
 fi
 
-BCONDS=$(./builder --show-bcond-args $specfile)
+BCONDS=$(./builder -nn -ncs --show-bcond-args $specfile)
 
 rpmbuild -bc "$@"
