0a30d21be34f555eda58e331c06af60782f72f38
Elan Ruusamäe <glen@pld-linux.org>
2009-04-23 10:00:23 +0000

    - update to 3.5.10
    Changed files:
        kdediff.sh -> 1.3

--- kdediff.sh
+++ kdediff.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-ver=3.5.9
+ver=3.5.10
+pkg="$1"
 
 # http://websvn.kde.org/tags/KDE/3.5.5/
 # http://websvn.kde.org/branches/KDE/3.5/
 # svn://anonsvn.kde.org/home/kde/trunk/KDE/kdelibs
 
 svn diff \
-	svn://anonsvn.kde.org/home/kde/tags/KDE/${ver}/$1 \
-	svn://anonsvn.kde.org/home/kde/branches/KDE/3.5/$1 \
-	> $1-branch.diff
+	svn://anonsvn.kde.org/home/kde/tags/KDE/${ver}/$pkg \
+	svn://anonsvn.kde.org/home/kde/branches/KDE/3.5/$pkg \
+	> $pkg-branch.diff
