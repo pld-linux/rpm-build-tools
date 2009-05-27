d85b44c56afd61261875bda5422c85f073df2c23
Michał 'Wolvverine' Panasiewicz <wolvverine@pld-linux.org>
2009-05-27 17:49:18 +0000

    - add CVSROOT
    Changed files:
        get-all-specs.sh -> 1.3

--- get-all-specs.sh
+++ get-all-specs.sh
@@ -2,4 +2,12 @@
 # get only *.spec files from packages/
 # thx SamChi,pawelz
 
-cd .. ; wget http://cvs.pld-linux.org/cgi-bin/cvsweb.cgi/packages/ -O - | sed '/<a href="\./!d' | sed 's,^.*<a href="\./\(.*\)/".*$,\packages/\1/\1.spec,' | xargs cvs get
+# run script in ./packages directory
+
+# change cvs to your login name
+CVSUSER=cvs
+
+export CVSROOT=:pserver:"${CVSUSER}"@cvs.pld-linux.org:/cvsroot
+cvs login
+cd .. 
+wget http://cvs.pld-linux.org/cgi-bin/cvsweb.cgi/packages/ -O - | sed '/<a href="\./!d' | sed 's,^.*<a href="\./\(.*\)/".*$,/\1/\1.spec,' | xargs cvs get
