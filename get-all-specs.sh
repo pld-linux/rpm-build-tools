84b866a1f478cb0fb323c9cdb80942937ed1a3bb
Michał 'Wolvverine' Panasiewicz <wolvverine@pld-linux.org>
2009-05-27 16:47:08 +0000

    - get all specs
    Changed files:
        get-all-specs.sh -> 1.1

---
+++ get-all-specs.sh
@@ -0,0 +1,5 @@
+#!bin/sh
+# get only *.spec files from packages/
+# thx SamChi,pawelz
+
+cd .. ; wget http://cvs.pld-linux.org/cgi-bin/cvsweb.cgi/packages/ -O - | sed '/<a href="\./!d' | sed 's,^.*<a href="\./\(.*\)/".*$,\packages/\1/\1.spec,' | xargs cvs get
