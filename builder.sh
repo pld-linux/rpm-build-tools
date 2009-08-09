4ee82499412a9dca27a5fc2401161dce660f6471
Arkadiusz Miśkiewicz <arekm@maven.pl>
2009-08-09 19:42:26 +0000

    - make sure that REPO_DIR exists
    Changed files:
        builder.sh -> 1.589

--- builder.sh
+++ builder.sh
@@ -663,6 +663,7 @@ init_builder() {
 		TOP_DIR=$(eval $RPM $RPMOPTS --eval '%{_topdir}')
 		REPO_DIR=$TOP_DIR/packages
 		PACKAGE_DIR=$TOP_DIR/packages/$ASSUMED_NAME
+		[ ! -d "$REPO_DIR" ] && install -d "$REPO_DIR"
 	else
 		REPO_DIR="."
 		PACKAGE_DIR="."
