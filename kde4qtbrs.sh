7588875542425ae21b9c3ee25e1b3fd0659d7c2c
Elan Ruusamäe <glen@pld-linux.org>
2010-02-22 08:29:53 +0000

    - no bashism, formatting, brief desc, use secure tmp
    Changed files:
        kde4qtbrs.sh -> 1.2

--- kde4qtbrs.sh
+++ kde4qtbrs.sh
@@ -1,18 +1,28 @@
-#!/bin/bash
+#!/bin/sh
 #
-# You need to install all Qt*-devel packages
+# Inspect package source and find Qt related build dependencies.
 #
-# auth: shadzik@pld-linux.org
+# NOTE: You need to install all Qt*-devel packages for the script to report
+# success.
+#
+# Author: shadzik@pld-linux.org
 
-if [ "$1" == "" ]; then
+if [ $# = 0 ]; then
 	echo "Usage: $0 <kdemodule>-<version>"
-	exit 0
+	exit 1
 fi
 
-rm -f /tmp/qtbrs
-HEADERS=$(grep -E -r '^#include\ <Q.*' BUILD/$1 |awk '{print $2}' |sort -u |sed -e 's/<//g;s/>//g')
+t=$(mktemp)
+rm -f $t
+HEADERS=$(grep -E -r '^#include\ <Q.*' BUILD/$1 | awk '{print $2}' | sort -u | sed -e 's/<//g;s/>//g')
 
-for i in $HEADERS; do find /usr/include/qt4 -print |grep $i |xargs rpm -qf >>/tmp/qtbrs; done
+for i in $HEADERS; do
+   	find /usr/include/qt4 -print | grep $i |xargs rpm -qf >> $t
+done
 
-for i in $(cat /tmp/qtbrs |sort -u); do ver=$(echo $i|sed -e 's/[a-zA-Z].*-devel-//g;s/-[0-9].*//g');i=$(echo $i|sed -e "s/-[0-9].*//g");echo -e "BuildRequires:\t$i >= $ver"; done
-rm -f /tmp/qtbrs
+for i in $(cat $t | sort -u); do
+	ver=$(echo $i | sed -e 's/[a-zA-Z].*-devel-//g;s/-[0-9].*//g')
+	i=$(echo $i | sed -e "s/-[0-9].*//g")
+	echo -e "BuildRequires:\t$i >= $ver"
+done
+rm -f $t
