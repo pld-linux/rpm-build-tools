dbb9b05b65ca235fc62b6ed2e99c8af1ff653468
Elan Ruusamäe <glen@pld-linux.org>
2010-09-16 18:43:36 +0000

    - list by size, show total
    Changed files:
        clean-distfiles.sh -> 1.2

--- clean-distfiles.sh
+++ clean-distfiles.sh
@@ -7,7 +7,8 @@ dir=$(dirname "$0")
 cd "$dir"
 
 ext=bz2,gz,rar,tgz,tbz2,zip,jar,Z,tar,png,ico,xpm,gif,rpm,bin,run,exe,iso,xpi,ZIP,dll
-ls -ld */*.{$ext}
+ls -ldrSh */*.{$ext}
+echo */*.{$ext} | xargs stat -c %s | awk '{s+=$1} END {printf("Total: %d MiB\n", s/1014/1024)}'
 
 echo remove? ctrl+c to abort
 read a
