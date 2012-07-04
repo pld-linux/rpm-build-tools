49cd827afc3259db117b6bc37bcb9b43ed7760d6
Elan Ruusamäe <glen@pld-linux.org>
2012-07-04 20:37:31 +0000

    - tool to update geoip packages
    Changed files:
        update-geoip.sh -> 1.1

---
+++ update-geoip.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -x
+set -e
+pkgs='GeoIP-db-City GeoIP-db-Country GeoIP-db-IPASNum xtables-geoip'
+for pkg in ${*:-$pkgs}; do
+	./builder -g -ns $pkg
+	cd $pkg
+	rm -vf *.gz *.zip
+	specfile=*.spec
+	../md5 $specfile
+	version=$(awk '/^Version:[ 	]+/{print $NF}' $specfile)
+	if [ $pkg = xtables-geoip ]; then
+		dt=$(stat -c %y *.zip | awk '{print $1}' | tr -d -)
+	else
+		dt=$(stat -c %y *.gz | awk '{print $1}' | tr - .)
+	fi
+	if [ "$version" != "$dt" ]; then
+		version=$dt
+		sed -i -e "
+			s/^\(Version:[ \t]\+\)[.0-9]\+\$/\1$version/
+		" $specfile
+	fi
+
+	../builder -bb *.spec
+	cd ..
+done
