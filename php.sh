6686afebdd9aad65218e611f76cd704d9f915786
Elan Ruusamäe <glen@delfi.ee>
2014-07-10 19:58:52 +0300

    php: cleanups

--- php.sh
+++ php.sh
@@ -2,9 +2,6 @@
 program=${0##*/}
 program=${program%.sh}
 dir=$(dirname "$0")
-rpmdir=$(rpm -E %_topdir)
-dist=th
 suffix=${program#php}
-#post_command="poldek -ev --noask php$suffix-devel"
 
-exec $dir/make-request.sh -D "php_suffix $suffix" ${post_command:+-C "$post_command"} -n "$@"
+exec $dir/make-request.sh -D "php_suffix $suffix" ${post_command:+-C "$post_command"} "$@"
