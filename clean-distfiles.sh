e6c4996b7ad31ccf96553fc5fb9307d2c2d9aff7
Elan Ruusamäe <glen@delfi.ee>
2014-11-23 02:07:24 +0200

    clean-distfiles: cosmetic

--- clean-distfiles.sh
+++ clean-distfiles.sh
@@ -25,7 +25,7 @@ ext=bz2,gz,rar,tgz,tbz2,zip,jar,Z,tar,png,ico,xpm,gif,rpm,bin,run,exe,iso,xpi,ZI
 ls -ldrSh */*.{$ext} || :
 echo */*.{$ext} | xargs stat -c %s | awk '{s+=$1} END {printf("Total: %d MiB\n", s/1014/1024)}'
 
-echo remove? ctrl+c to abort
+echo Move to $purgedir? Hit Ctrl+C to abort
 read a
 
 install -d $purgedir
