0ee59b3ffd12cfe392d3b9797f8f241867262c66
Elan Ruusamäe <glen@delfi.ee>
2015-02-26 23:40:40 +0200

    drop rpmb
    not needed either, rpm-build-macros can setup spec and sourcedirs properly
    
    apply 4bd523d4d38aa9b69d383ab41e722fbeed3c0b9a from rpm-build-macros repo

--- shrc.sh
+++ shrc.sh
@@ -262,15 +262,3 @@ d() {
 rpm2spec() {
 	sed -re 's,^(.+)-[^-]+-[^-]+$,\1.spec,'
 }
-
-rpmb() {
-	local SPEC SPECDIR arg
-	for arg in "$@"; do
-		case "$arg" in
-			*.spec)	SPEC="$arg"
-				;;
-		esac
-	done
-	SPECDIR=$(dirname "$(pwd)/${SPEC:-.}")
-	command rpmbuild --define "_specdir $SPECDIR" --define "_sourcedir $SPECDIR" "$@"
-}
