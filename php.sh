e2f77c160041c2b2757bdb6988c0dcf8e4fbff72
Elan Ruusamäe <glen@delfi.ee>
2015-05-08 21:26:38 +0300

    if called as php.sh, invoke all php versions

--- php.sh
+++ php.sh
@@ -4,4 +4,12 @@ program=${program%.sh}
 dir=$(dirname "$0")
 suffix=${program#php}
 
+# if called as php.sh, invoke all php versions
+if [ "$suffix" = "" ]; then
+	for php in $dir/php??.sh; do
+		$php "$@"
+	done
+	exit 0
+fi
+
 exec $dir/make-request.sh -D "php_suffix $suffix" ${post_command:+-C "$post_command"} "$@"
