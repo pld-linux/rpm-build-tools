cee2723afb7bfefd5d578e345157a7b78df243df
Elan Ruusamäe <glen@delfi.ee>
2016-07-04 14:47:21 +0300

    php: add pre_command

--- php.sh
+++ php.sh
@@ -10,6 +10,8 @@ if [ "$suffix" = "" ]; then
 		$php "$@"
 	done
 	exit 0
+else
+	pre_command='for a in php4-common php52-common php53-common php54-common php55-common php56-common php70-common hhvm; do poldek -e $a --noask; done'
 fi
 
-exec $dir/make-request.sh -D "php_suffix $suffix" ${post_command:+-C "$post_command"} "$@"
+exec $dir/make-request.sh -D "php_suffix $suffix" ${pre_command:+-c "$pre_command"} ${post_command:+-C "$post_command"} "$@"
